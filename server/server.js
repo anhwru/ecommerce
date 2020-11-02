14195491bf15b68884317e1b3328e80238d4d980
DESKTOP-A5OB1BH\anhnv <anhnv620@wru.vn>
2020-11-03 01:56:53 +0700

    Login api

--- server/server.js
+++ server/server.js
@@ -9,13 +9,64 @@ app.use(cors(corsOptions));
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 // simple route
 app.get("/", (req, res) => {
-    res.json({ message: "Welcome to CBR VietNam" });
+    res.json({message: "Welcome to CBR VietNam"});
 });
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
+// routes
+require('./routes/auth.routes')(app);
+require('./routes/user.routes')(app);
+
+const db = require("./models");
+const Role = db.role;
+db.mongoose
+    .connect(`mongodb://localhost:27017/Ecommerce`, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => {
+        console.log("Successfully connect to MongoDB.");
+        initial();
+    })
+    .catch(err => {
+        console.error("Connection error", err);
+        process.exit();
+    });
+
+// ket noi DB
+function initial() {
+    Role.estimatedDocumentCount((err, count) => {
+        if (!err && count === 0) {
+            new Role({
+                name: "user"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'user' to roles collection");
+            });
+            new Role({
+                name: "moderator"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'moderator' to roles collection");
+            });
+            new Role({
+                name: "admin"
+            }).save(err => {
+                if (err) {
+                    console.log("error", err);
+                }
+                console.log("added 'admin' to roles collection");
+            });
+        }
+    });
+}
